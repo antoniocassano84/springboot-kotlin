2e3210be51b423bcb7fafade3a89c61da62228ac
Antonio Cassano <Antonio.Cassano@nhsbsa.nhs.uk>
2023-12-29 14:56:38 +0000

    articles instead

--- src/main/kotlin/com/jetbrains/marcocodes/springbootkotlin/RestControllers.kt
+++ src/main/kotlin/com/jetbrains/marcocodes/springbootkotlin/RestControllers.kt
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/v1/article")
+@RequestMapping("/api/v1/articles")
 class ArticleController {
 
     val articles = mutableListOf(Article("My Title", "my content"))
