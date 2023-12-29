c98ea7fbb3792a49c86670e3ccbbfcfe05e1fc7a
Antonio Cassano <Antonio.Cassano@nhsbsa.nhs.uk>
2023-12-29 14:39:06 +0000

    Initial commit

---
+++ src/test/kotlin/com/jetbrains/marcocodes/springbootkotlin/SpringbootKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jetbrains.marcocodes.springbootkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
