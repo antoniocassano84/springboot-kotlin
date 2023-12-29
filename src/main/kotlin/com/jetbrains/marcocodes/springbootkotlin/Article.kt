d8c39f258f29288a970b932612d5bf64f07e8acc
antonio <ac.pollazzo@gmail.com>
2023-12-29 16:14:27 +0000

    completed rest controller for Article

--- src/main/kotlin/com/jetbrains/marcocodes/springbootkotlin/Article.kt
+++ src/main/kotlin/com/jetbrains/marcocodes/springbootkotlin/Article.kt
@@ -3,8 +3,8 @@ package com.jetbrains.marcocodes.springbootkotlin
 import java.time.LocalDateTime
 
 data class Article(
-    val title: String,
-    val content: String,
-    val createdAt: LocalDateTime = LocalDateTime.now(),
-    val slug: String = title.toSlug()
+        var title: String,
+        var content: String,
+        var createdAt: LocalDateTime = LocalDateTime.now(),
+        var slug: String = title.toSlug()
 )
--- src/main/kotlin/com/jetbrains/marcocodes/springbootkotlin/RestControllers.kt
+++ src/main/kotlin/com/jetbrains/marcocodes/springbootkotlin/RestControllers.kt
@@ -1,8 +1,15 @@
 package com.jetbrains.marcocodes.springbootkotlin
 
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("/api/v1/articles")
@@ -12,5 +19,27 @@ class ArticleController {
 
     @GetMapping
     fun articles() = articles
+
+    @GetMapping("/{slug}")
+    fun article(@PathVariable slug: String) =
+            articles.find { article -> article.slug == slug } ?: throw ResponseStatusException(HttpStatus.NOT_FOUND)
+
+    @PostMapping
+    fun newArticle(@RequestBody article: Article): Article {
+        articles.add(article)
+        return article
+    }
+
+    @PutMapping("/{slug}")
+    fun updateArticle(@RequestBody article: Article, @PathVariable slug: String): Article {
+        val existingArticle = articles.find { it.slug == slug } ?: throw ResponseStatusException(HttpStatus.NOT_FOUND)
+        existingArticle.content = article.content
+        return article
+    }
+
+    @DeleteMapping("/{slug}")
+    fun removeArticle(@PathVariable slug: String) {
+        articles.removeIf { article -> article.slug == slug }
+    }
 
 }
